96b9375c4a4ce0b63164e168a6e062a3bdd87404
Abubakar-yerbour <abubakarbello3914@gmail.com>
2026-04-28 22:52:24 +0100

    bump version to 3.0.1

--- ugclaw/__init__.py
+++ ugclaw/__init__.py
@@ -4,7 +4,7 @@ Author : Abubakar Bello <abubakarbello3914@gmail.com>
 License: MIT
 """
 
-__version__ = "3.1.0"
+__version__ = "3.0.1"
 __author__  = "Abubakar Bello"
 __email__   = "abubakarbello3914@gmail.com"
 __license__ = "MIT"
